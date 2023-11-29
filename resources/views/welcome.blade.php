43cfef5074a49c010bd9da6584ab1fca078d6cc2
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2023-11-29 10:04:12 +0600

    Some Changes From Apon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.default')
 @section('content')
 
-<div class="content-header">
-    <div class="breadcrumb">
-        <a class="page current" href="dashboard.php" data-i18n="dashboard"></a>
-    </div>
+<div>
+    <h1>Mark2</h1>
+    <p>Mark2 is a simple content management system</p>
+    <p>{{ __('messages.Dashboard') }}</p>
 </div>
-<div class="content-main">
-    <div data-i18n="dashboard"></div>
-</div>
+
 
 @stop
